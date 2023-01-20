c793c852ba2505df48f97f003e7bff6a421321c6
Kim-Haul <bokugawa@naver.com>
2023-01-20 20:54:16 +0900

    :memo: 객체 중복 생성 문제 해결 (멤버 변수로 선언)

--- src/main/java/com/example/springcore/simplerefactoring/ProductController.java
+++ src/main/java/com/example/springcore/simplerefactoring/ProductController.java
@@ -12,10 +12,19 @@ import java.util.List;
 @RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
+
+    private final ProductService productService;
+
+    // 생성자
+    public ProductController() {
+        ProductService productService = new ProductService();
+        this.productService = productService;
+    }
+
     // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
+//        ProductService productService = new ProductService();
         Product product = productService.createProduct(requestDto);
 
     // 응답 보내기
@@ -25,7 +34,7 @@ public class ProductController {
     // 설정 가격 변경
     @PutMapping("/api/products/{id}")
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
+//        ProductService productService = new ProductService();
         Product product = productService.updateProduct(id, requestDto);
 
     // 응답 보내기 (업데이트된 상품 id)
@@ -35,7 +44,7 @@ public class ProductController {
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts() throws SQLException {
-        ProductService productService = new ProductService();
+//        ProductService productService = new ProductService();
         List<Product> products = productService.getProducts();
         
     // 응답 보내기
--- src/main/java/com/example/springcore/simplerefactoring/ProductService.java
+++ src/main/java/com/example/springcore/simplerefactoring/ProductService.java
@@ -9,11 +9,21 @@ import java.util.List;
 
 // 아직은 스프링이 제공하는 @Service 사용 하지 않고 !!
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    // 생성자
+    public ProductService() {
+        ProductRepository productRepository = new ProductRepository();
+        this.productRepository = productRepository;
+    }
+
+
     public Product createProduct(ProductRequestDto requestDto) throws SQLException {
         // 요청받은 DTO 로 DB에 저장할 객체 만들기
         Product product = new Product(requestDto);
 
-        ProductRepository productRepository = new ProductRepository();
+//        ProductRepository productRepository = new ProductRepository();
         productRepository.createProduct(product);
 
         return product;
@@ -25,7 +35,7 @@ public class ProductService {
             throw new RuntimeException("희망 최저가는 0 이상으로 설정해주세요.");
         }
 
-        ProductRepository productRepository = new ProductRepository();
+//        ProductRepository productRepository = new ProductRepository();
         Product product = productRepository.getProduct(id);
 
         if (product == null) {
@@ -38,7 +48,7 @@ public class ProductService {
     }
 
     public List<Product> getProducts() throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
+//        ProductRepository productRepository = new ProductRepository();
         List<Product> products = productRepository.getProducts();
 
         return products;
